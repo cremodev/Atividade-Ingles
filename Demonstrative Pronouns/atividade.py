10f5473361924339e25493dc02f7e0214f0fc977
cremodev <osilveira.renan@gmail.com>
2022-05-27 20:18:53 -0300

    Update-Renan

--- Demonstrative Pronouns/atividade.py
+++ Demonstrative Pronouns/atividade.py
@@ -1,27 +1,65 @@
-from dicionario import perguntas
-
-print('''Olá, esse é um exercício para testar suas habilidades com demonstrative pronouns
-
-Temos 8 perguntas para você responder
-Preencha os espaços em branco ''')
-print('-=-' * 15)
-
-respostas_certas = 0
-
-for pkey, pvalue in perguntas.items():
-    print(f'{pkey} : {pvalue["pergunta"]}')
-    print('-=-'*13)
+import pandas as pd
+from random import choice
 
 
-    resposta_user = input('Responda com [This ou That]: ').strip().upper()
+print('''\033[;1mHello, this is an exercise to test your skills with Demonstrative Pronouns
+Fill in the blanks with [THIS-THAT-THESE-THOSE]\033[m ''')
+print('-=-'*20)
+
+def sorteia(dd):
+    t = 'ok'
+    while t == 'ok':
+        try:
+            newdd = (choice(dd))
+            t = 'foi'
+        except:
+            pass
+    return newdd
+
+
+def importa_planilha(colunas):
+    url_file = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTYnk1dsVTyXxx9-2mbarCEayKhozmJMFoLH3mTE7gl_mL2uZBrHuEXudqUuAxpzcNHxMNagLMDmpON/pub?output=csv'
+    return pd.read_csv(url_file, index_col=0, header=0, usecols=colunas).to_dict('index')
+
+def ln():
+    for i in range(20):       
+        print('-=-', end='')
     print()
+    
+colunas = list(['ID', 'Perguntas', 'Respostas'])
+dd = importa_planilha(colunas)
 
-    if resposta_user == pvalue["resposta_certa"]:
-        print('\033[1;32mPARABÉNS, VOCÊ ACERTOU!!!!\033[m')
-        print('-=-'*13)
-        respostas_certas += 1
+resp_pron = 0
+resp = ''
+qtd_perguntas = 0
+
+while resp != '999':
+    qtd_perguntas += 1
+    uma_pergunta = sorteia(dd)
+    pergunta = uma_pergunta['Perguntas']
+    resposta_certa = uma_pergunta['Respostas']
+
+    #ln()
+    print(pergunta)
+    ln()
+
+    resposta_certa = str(resposta_certa).strip().upper()
+    # print("Resposta certa é:", resposta_certa)
+    resp = input("Answer with the proper pronoun or type 999 to exit: ").strip().title()
+    resp = str(resp).strip().upper()
+
+    # print('Resposta digitada foi: ', resp)
+
+    ln()
+    if resp == resposta_certa:
+        resp_pron += 1
+        print(f"\033[1;32mCONGRATS YOU ALREADY GET {resp_pron} ANSWERS RIGHT\033[m")
     else:
-        print('\033[1;31mRESPOSTA ERRADA, BOA SORTE NA PRÓXIMA\033[m')
-        print('-=-'*13)
+        print("\033[1;31mWHAT A PITY ... GOOD LUCK NEXT TIME\033[m")
 
-print('Muito Obrigado por usar nosso programa :)')
+    ln()
+
+
+print(
+    f'You tested {qtd_perguntas} questions, \033[1;32mnailed {resp_pron}\033[m and \033[1;31mmissed { qtd_perguntas - resp_pron}\033[m')
+ln()
